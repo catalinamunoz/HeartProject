64b60ddad633779c8ba6d21ab48e07b2b5373e8e
catalinamunoz <cmm2228@nau.edu>
2023-02-17 13:56:58 -0700

    Updated data documentation

--- R/heart_disease.R
+++ R/heart_disease.R
@@ -7,24 +7,24 @@
 #'
 #' @format A data frame with 54,746 observations and 18 columns.
 #' \describe{
-#'    \item{HeartDisease}{Has the respondent had coronary heart disease or myocardial infarction?}
+#'    \item{HeartDisease}{Has the respondent had coronary heart disease or myocardial infarction? (Yes/No)}
 #'    \item{BMI}{Body Mass Index}
-#'    \item{Smoking}{ Have they smoked 100 cigarettes in their lifetime? }
-#'    \item{AlcoholDrinking}{Are they a heavy drinker? Men: More then 14 drinks per week, Women: More than 7}
-#'    \item{Stroke}{Have they had a stroke in their lifetime?}
+#'    \item{Smoking}{ Have they smoked 100 cigarettes in their lifetime? (Yes/No)}
+#'    \item{AlcoholDrinking}{Are they a heavy drinker? Men: More than 14 drinks per week, Women: More than 7 (Yes/No)}
+#'    \item{Stroke}{Have they had a stroke in their lifetime? (Yes/No)}
 #'    \item{PhysicalHealth}{How many days in the last 30 was their physical health bad?}
 #'    \item{MentalHealth}{How many days in the last 30 was their mental health bad?}
-#'    \item{DiffWalking}{Is it difficult to walk or climb the stairs?}
+#'    \item{DiffWalking}{Is it difficult to walk or climb the stairs? (Yes/No)}
 #'    \item{Sex}{Male or Female}
 #'    \item{AgeCategory}{13 level age category ranging from 18 to 80 years or older}
 #'    \item{Race}{Race/Ethnicity}
-#'    \item{Diabetic}{Have they had diabetes?}
-#'    \item{PhysicalActivity}{Have they reported exercise in the past 30 days?}
-#'    \item{GenHealth}{In general, how good do they think their health is?}
+#'    \item{Diabetic}{Have they had diabetes? (Yes, No, Borderline, During Pregnancy)}
+#'    \item{PhysicalActivity}{Have they reported exercise in the past 30 days? (Yes/No)}
+#'    \item{GenHealth}{In general, how good do they think their health is? (Poor, Fair, Good, Very Good, Excellent) }
 #'    \item{SleepTime}{How many hours of sleep do they get per day?}
-#'    \item{Asthma}{Have they had asthma?}
-#'    \item{KidneyDisease}{Have they had kidney disease?}
-#'    \item{SkinCancer}{Have they had skin cancer?}
+#'    \item{Asthma}{Have they had asthma? (Yes/No)}
+#'    \item{KidneyDisease}{Have they had kidney disease? (Yes/No)}
+#'    \item{SkinCancer}{Have they had skin cancer? (Yes/No)}
 #' }
 #' @source \url{https://www.kaggle.com/datasets/kamilpytlak/personal-key-indicators-of-heart-disease}
 "heart_disease"
